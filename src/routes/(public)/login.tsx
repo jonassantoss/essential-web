6a8a407b5ce3dc3de7d418f09c453dadfd323638
Jonas Santos <jonassoares.live@gmail.com>
2025-05-21 19:21:02 -0300

    temp: tela de login

--- src/routes/(public)/login.tsx
+++ src/routes/(public)/login.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { useForm } from "@tanstack/react-form";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -5,7 +6,6 @@ export const Route = createFileRoute("/(public)/login")({
   component: LoginPage,
 });
 
-// TODO Rota de Login ainda não completamente implementada
 function LoginPage() {
   const form = useForm({
     defaultValues: {
@@ -23,10 +23,11 @@ function LoginPage() {
   };
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-center items-center space-y-8">
       <h1 className="text-2xl font-medium">Login</h1>
-      <div className="w-1/6">
+      <div className="w-1/6 bg-white p-6 rounded-lg shadow-2xl">
         <form
+          className="flex flex-col space-y-4"
           onSubmit={(e) => {
             e.preventDefault();
             e.stopPropagation();
@@ -38,18 +39,46 @@ function LoginPage() {
             children={(field) => {
               return (
                 <div className="flex flex-col">
-                  <label htmlFor={field.name}>Email</label>
+                  <label className="text-md" htmlFor={field.name}>
+                    Email:
+                  </label>
                   <input
-                    className="p-1 rounded-sm border border-gray-300"
+                    className="relative p-1 rounded-sm border border-gray-300 outline-none focus:border focus:border-sky-300"
                     type="email"
                     name={field.name}
                     id={field.name}
                     value={field.state.value}
                     onBlur={field.handleBlur}
                     onChange={(e) => field.handleChange(e.target.value)}
-                  />
+                  ></input>
                 </div>
               );
+            }}
+          />
+          <form.Field
+            name="password"
+            children={(field) => {
+              return (
+                <div className="flex flex-col">
+                  <label className="text-md" htmlFor={field.name}>
+                    Senha:
+                  </label>
+                  <input
+                    className="relative p-1 rounded-sm border border-gray-300 outline-none focus:border focus:border-sky-300"
+                    type="password"
+                    name={field.name}
+                    id={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  ></input>
+                </div>
+              );
+            }}
+          />
+          <form.Subscribe
+            children={(field) => {
+              return <Button>Entrar</Button>;
             }}
           />
         </form>
