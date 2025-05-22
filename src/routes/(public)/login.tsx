07fc827afdbe3acb420dfe4e14c9960998aaacef
jonassantoss <jonassoares.live@gmail.com>
2025-05-21 21:32:06 -0300

    feat: finaliza tela de login

--- src/routes/(public)/login.tsx
+++ src/routes/(public)/login.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { loginSchema } from "@/validation/schemas";
 import { useForm } from "@tanstack/react-form";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -16,6 +17,9 @@ function LoginPage() {
       const { email, password } = value;
       handleSubmit(email, password);
     },
+    validators: {
+      onChange: loginSchema,
+    },
   });
 
   const handleSubmit = (email: string, password: string) => {
@@ -24,8 +28,11 @@ function LoginPage() {
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center space-y-8">
-      <h1 className="text-2xl font-medium">Login</h1>
-      <div className="w-1/6 bg-white p-6 rounded-lg shadow-2xl">
+      <div className="min-w-[400px] flex flex-col space-y-2 bg-white p-6 rounded-lg shadow-my-card">
+        <header className="flex flex-col space-y-1">
+          <h2 className="text-2xl font-semibold text-center">Login</h2>
+          <p className="text-sm text-gray-600 text-center">Acesse sua conta</p>
+        </header>
         <form
           className="flex flex-col space-y-4"
           onSubmit={(e) => {
@@ -34,53 +41,71 @@ function LoginPage() {
             form.handleSubmit();
           }}
         >
-          <form.Field
-            name="email"
-            children={(field) => {
-              return (
-                <div className="flex flex-col">
-                  <label className="text-md" htmlFor={field.name}>
-                    Email:
-                  </label>
-                  <input
-                    className="relative p-1 rounded-sm border border-gray-300 outline-none focus:border focus:border-sky-300"
-                    type="email"
-                    name={field.name}
-                    id={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  ></input>
-                </div>
-              );
-            }}
-          />
-          <form.Field
-            name="password"
-            children={(field) => {
-              return (
-                <div className="flex flex-col">
-                  <label className="text-md" htmlFor={field.name}>
-                    Senha:
-                  </label>
-                  <input
-                    className="relative p-1 rounded-sm border border-gray-300 outline-none focus:border focus:border-sky-300"
-                    type="password"
-                    name={field.name}
-                    id={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  ></input>
-                </div>
-              );
-            }}
-          />
-          <form.Subscribe
-            children={(field) => {
-              return <Button>Entrar</Button>;
-            }}
-          />
+          <form.Field name="email">
+            {(field) => (
+              <div className="flex flex-col">
+                <label className="text-md" htmlFor={field.name}>
+                  Email:
+                </label>
+                <input
+                  className="relative p-1 rounded-sm border border-gray-300 outline-none focus:border focus:border-primary"
+                  style={{
+                    borderColor:
+                      field.state.meta.errors.length > 1
+                        ? "#e7000b"
+                        : field.state.meta.isValid
+                        ? "#13284C"
+                        : "",
+                  }}
+                  type="email"
+                  name={field.name}
+                  id={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {!field.state.meta.isValid && (
+                  <p className="text-destructive italic" role="alert">
+                    {field.state.meta.errors[0]?.message}
+                  </p>
+                )}
+              </div>
+            )}
+          </form.Field>
+          <form.Field name="password">
+            {(field) => (
+              <div className="flex flex-col">
+                <label className="text-md" htmlFor={field.name}>
+                  Senha:
+                </label>
+                <input
+                  className="relative p-1 rounded-sm border border-gray-300 outline-none focus:border focus:border-primary"
+                  style={{
+                    borderColor:
+                      field.state.meta.errors.length > 1
+                        ? "#e7000b"
+                        : field.state.meta.isValid
+                        ? "#13284C"
+                        : "",
+                  }}
+                  type="password"
+                  name={field.name}
+                  id={field.name}
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {!field.state.meta.isValid && (
+                  <p className="text-destructive italic" role="alert">
+                    {field.state.meta.errors[0]?.message}
+                  </p>
+                )}
+              </div>
+            )}
+          </form.Field>
+          <form.Subscribe>
+            <Button type="submit">Entrar</Button>
+          </form.Subscribe>
         </form>
       </div>
     </div>
